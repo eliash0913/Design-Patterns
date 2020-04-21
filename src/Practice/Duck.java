4e6ed624b5fb703d4de55f8634e6d873d76b65c6
eliash0913 <eliash0913@gmail.com>
2020-04-21 08:45:26 -0700

    Basic Structure

---
+++ src/Practice/Duck.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+
+    public void setFlyBehavior(FlyBehavior fb){
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb){
+        quackBehavior = qb;
+    }
+
+    abstract void display();
+
+    public void swim(){
+        System.out.println("Swimming.");
+    }
+
+}
---
+++ src/Practice/FlyWithWings.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class FlyWithWings implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("Flying!");
+    }
+}
